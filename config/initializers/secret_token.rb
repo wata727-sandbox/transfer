d9cb26a8beefda6fd4f8c1837aff88352ef68c45
Tsuyoshi Yonemoto <tsuyoshi.yonemoto@gmail.com>
2014-01-28 18:08:12 +0900

    わざとスペースを含んだコミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hogehoge::Application.config.secret_key_base = '5e668c9de6270b438a5a69d154b8d8c32b509c46cccd41a56fc121a48c97fd64944e0c18927dcdc82371cc9ba664174188b656020fd45c3ef1237cc2888b9303'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hogehoge::Application.config.session_store :cookie_store, key: '_hogehoge_session'

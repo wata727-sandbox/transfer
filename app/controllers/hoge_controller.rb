8dddfbd475293e87a592498b86ac865be7481f39
Tsuyoshi Yonemoto <tsuyoshi.yonemoto@gmail.com>
2014-01-30 11:43:56 +0900

    hogeを追加

---
+++ app/controllers/hoge_controller.rb
@@ -0,0 +1,37 @@
+class HogeController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  #
+  #aaaaaa
+  #
+  #
+  
+  #
+  #
+  #
+  #
+  #
+  #
+#
+  #
+  #
+  #
+  #
+  #
+#
+  #
+  #
+  #
+  #
+  #
+  
+  #
+  #
+#
+  #
+  #
+  #
+  #
+  
+  #aa
+end

23d1d5201509405caa67246466a15d98b3429495
Tsuyoshi Yonemoto <tsuyoshi.yonemoto@gmail.com>
2014-03-02 22:21:57 +0900

    ほげほげ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # 
+  # 
   def index3
   end
   def index2

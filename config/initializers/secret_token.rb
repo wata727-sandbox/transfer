25b9297edc13b8e469aa7dae66b1c05b4f5ab6ee
Tsuyoshi Yonemoto <tsuyoshi.yonemoto@gmail.com>
2014-03-13 12:38:30 +0900

    test

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,5 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Hogehoge::Application.config.secret_key_base = '5e668c9de6270b438a5a69d154b8d8c32b509c46cccd41a56fc121a48c97fd64944e0c18927dcdc82371cc9ba664174188b656020fd45c3ef1237cc2888b9303'  
+Hogehoge::Application.config.secret_key_base = '5e668c9de6270b438a5a69d154b8d8c32b509c46cccd41a56fc121a48c97fd64944e0c18927dcdc82371cc9ba664174188b656020fd45c3ef1237cc2888b9303'
 

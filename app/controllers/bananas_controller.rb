607b09eb6016fbe773caf399720231d198de4669
Tsuyoshi Yonemoto <tsuyoshi.yonemoto@gmail.com>
2014-03-14 23:37:59 +0900

    aaaaaaaa

--- app/controllers/bananas_controller.rb
+++ app/controllers/bananas_controller.rb
@@ -3,6 +3,7 @@ class BananasController < ApplicationController
   before_action :set_banana, only: [:show, :edit, :update, :destroy]
   
   # GET /bananas
+  # 
   # GET /bananas.json
   def index
     @bananas = Banana.all

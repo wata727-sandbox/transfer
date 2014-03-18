0cb9269f32ea5975a3033dafe0403811b4e31f6e
Koichiro Sumi <koichiro.sumi@actcat.co.jp>
2014-03-18 18:48:42 +0900

    waste_actionをunusedにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
- before_filter :waste_action # Not waste!
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
